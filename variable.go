9b9c1ce8181aa7c914021ac78b05ae235e68fa9a
Hafiz Irwandi <hafizirwan@gmail.com>
2023-03-28 15:49:03 +0700

    hello word

---
+++ variable.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	//membuat variable di go bisa menggunakan kata kunci "var" atau ":="
+	//dengan var
+	var name ="Hafiz Irwandi"
+	fmt.Println("Hai, Nama",name)
+
+	name ="Prestika Dita Sahara"
+	fmt.Println("Hai, Nama Saya ",name)
+
+	//dengan :=
+	tempat_lahir:="Medan"
+	fmt.Println("Saya ",name," Lahir di ",tempat_lahir)
+}
+
